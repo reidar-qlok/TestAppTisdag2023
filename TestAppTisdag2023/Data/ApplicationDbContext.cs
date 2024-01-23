3d25f95dee7da3cba35576674c152f1dbdee64ed
Reidar Nilsen <4reidar@gmail.com>
2024-01-23 14:03:57 +0100

    Add project files.

---
+++ TestAppTisdag2023/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TestAppTisdag2023.Models;
+
+namespace TestAppTisdag2023.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet <Employee> Employees { get; set; }
+    }
+}
---
+++ TestAppTisdag2023/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAppTisdag2023.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+    }
+}
